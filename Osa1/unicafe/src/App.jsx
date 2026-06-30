a84bbd7ce0ecb9d5b0fe1c8883e3ca28be53614a
Esa Nord <esa.nord@hotmail.com>
2026-06-30 13:04:42 +0300

    Exercise 1.7

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0)             // +1
+    const [neutral, setNeutral] = useState(0)       // 0
+    const [bad, setBad] = useState(0)               // -1
 
     const handleGood = () => setGood(good + 1)
     const handleNeutral = () => setNeutral(neutral + 1)
@@ -20,6 +20,9 @@ const App = () => {
                 <dt>good {good}</dt>
                 <dt>neutral {neutral}</dt>
                 <dt>bad {bad}</dt>
+                <Total good={good} neutral={neutral} bad={bad} />
+                <Average good={good} neutral={neutral} bad={bad} />
+                <Positive good={good} neutral={neutral} bad={bad} />
             </dl>
             
         </div>
@@ -35,6 +38,34 @@ const Button = (props) => {
         <button onClick={props.onClick}>
             {props.text}
         </button>
+    )
+}
+
+const Total = ({ good, neutral, bad }) => {
+    let total = good + neutral + bad
+    return (        
+        <dt>total {total}</dt>
+    )
+}
+
+const Average = ({ good, neutral, bad }) => {
+    let average = 0
+    if (good > 0 || neutral > 0 || bad > 0) {
+        average = (good - bad) / (good + neutral + bad)
+    }
+
+    return (
+        <dt>average {average}</dt>
+    )
+}
+
+const Positive = ({ good, neutral, bad }) => {
+    let positive = 0
+    if (good > 0 || neutral > 0 || bad > 0) {
+        positive = (good / (good + neutral + bad)) * 100
+    }
+    return (
+        <dt>positive {positive} %</dt>
     )
 }
 
